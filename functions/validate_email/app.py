fc708e047c55c033f30720580efd8fc802eb6f13
James Shapiro <jamesshapiro@gmail.com>
2022-01-17 12:37:28 -0500

    add a validate email step to state machine

---
+++ functions/validate_email/app.py
@@ -0,0 +1,38 @@
+import boto3
+
+ses_client = boto3.client("ses")
+
+def send_email(ses_client, commenter_email):
+    charset = "UTF-8"
+    response = ses_client.send_email(
+        Destination={
+            "ToAddresses": [
+                commenter_email,
+            ],
+        },
+        Message={
+            "Body": {
+                "Text": {
+                    "Charset": charset,
+                    "Data": "validation email",
+                }
+            },
+            "Subject": {
+                "Charset": charset,
+                "Data": "validation email",
+            },
+        },
+        Source="JS Comment Validator <jamesshapirocomments+athens@gmail.com>",
+    )
+    return response
+
+def lambda_handler(event, context):
+    print(f'{event=}')
+    print(f'{context=}')
+    if 'COMMENTER_EMAIL' in event:
+        commenter_email = event['COMMENTER_EMAIL']
+    response = send_email(ses_client, commenter_email)
+    print(response)
+    return
+
+lambda_handler({}, None)
