11fc4234ad5e5d5d959cca63a619f1d32cbdd3a4
James Shapiro <jamesshapiro@gmail.com>
2022-01-18 00:43:53 -0500

    add API Gateway + Lambda backend to confirm the task token for the confirmation email

--- functions/register_email_confirmation/app.py
+++ functions/register_email_confirmation/app.py
@@ -1,45 +1,22 @@
 import boto3
 import os
+import json
 
-ses_client = boto3.client("ses")
-
-confirm_comment_endpoint = os.environ['CONFIRM_COMMENT_ENDPOINT']
-
-def send_email(ses_client, commenter_email, token):
-    charset = "UTF-8"
-    html_email_content = f"""
-        <html>
-            <head></head>
-            <h1 style='text-align:center'>This is the heading</h1>
-            <p>Hello, world</p>
-            <p>Please click <a href="{confirm_comment_endpoint}?token={token}">here</a>
-            </body>
-        </html>"""
-    response = ses_client.send_email(
-        Destination={
-            "ToAddresses": [
-                commenter_email,
-            ],
-        },
-        Message={
-            "Body": {
-                "Html": {
-                    "Charset": charset,
-                    "Data": html_email_content,
-                }
-            },
-            "Subject": {
-                "Charset": charset,
-                "Data": "validation email",
-            },
-        },
-        Source="JS Comment Validator <jamesshapirocomments+athens@gmail.com>",
-    )
-    return response
+stepfunctions_client = boto3.client("stepfunctions")
 
 def lambda_handler(event, context):
-    commenter_email = event['commenter_email']
-    token = event['token']
-    response = send_email(ses_client, commenter_email, token)
+    print(f'{event}=')
+    token = event['queryStringParameters']['token']
+    token = token.replace(' ','+')
+
+    print(f'{token[:100]=}')
+
+    response = stepfunctions_client.send_task_success(
+        taskToken=token,
+        output=json.dumps({"success": "success"})
+    )
     print(response)
-    return
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Comment verified, now pending approval from James!')
+    }
